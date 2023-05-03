a0fd013217d01170440b8beb82b3cba5e48730c4
dongeal <dongeal@gmail.com>
2023-05-03 13:00:28 +0900

    40:00회사에서

--- 4_keyboard_event.py
+++ 4_keyboard_event.py
@@ -21,6 +21,9 @@ character_height = character_size[1] #캐릭터 세로크기
 character_x_pos = (screen_width / 2) - (character_width / 2) # 화면 중앙
 character_y_pos = screen_height - character_height # 화면 의 가장 아래
 
+# 이동할 좌표
+to_x = 0
+to_y = 0
 
 # 이벤트 루프
 running = True  # 게임이 진행 중인가?
@@ -28,6 +31,38 @@ while running:
     for event in pygame.event.get(): # 어떤 이벤트가 발생?
         if event.type == pygame.QUIT: # 창이 닫히는 이벤트 발행?
             running = False #게임이 진행 중이 아님
+
+        if event.type == pygame.KEYDOWN: # 키가 눌렸는지 확이
+            if event.key ==  pygame.K_LEFT: # 캐릭터를 왼쪽으로 
+                to_x -= .5
+            elif event.key ==  pygame.K_RIGHT: # 캐릭터를 오른쪽으로 
+                to_x += .5
+            elif event.key ==  pygame.K_UP: # 캐릭터를 위로
+                to_y -= .5
+            elif event.key ==  pygame.K_DOWN: # 캐릭터를 아래로 
+                to_y += .5
+
+        if event.type == pygame.KEYUP: # 방향키 떼면 멈춤
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
+
+    character_x_pos += to_x
+    character_y_pos += to_y
+
+    #가로 경계값
+
+    if character_x_pos < 0 :
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    # 세로 경계값   
+    if character_y_pos < 0 :
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
 
     screen.blit(background,(0,0)) #배경 그리기
 
